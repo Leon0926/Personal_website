487527f64b7fc788ca83ae5e7297b51ee3b5ad0c
Leon's Laptop <102eon@gmail.com>
2022-11-11 15:58:09 -0800

    added constant array called courselist into script file.

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+let course1={
+    code: "ACIT 1620", 
+    name: "Web Fundamental Technologies"
+};
+let course2={
+    code: "ACIT 1515",
+    name: "Scripting for IT"
+};
+let course3={
+    code: "ACIT 1630",
+    name: "Database Systems"
+};
+
+let courseList = [course1, course2, course3];
+
